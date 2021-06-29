38067bdbe5683b499fee4a07b6c1d2c20e463609
lado at luka <tsivtsivadzelado@yahoo.com>
2021-06-29 10:35:23 -0700

    progressing

--- LadoTsivtsivadze_42/SecondPageController.swift
+++ LadoTsivtsivadze_42/SecondPageController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class SecondPageController: UIViewController {
-
+    
+    private let bezierPath = UIBezierPath()
+    private var shapeLayer = CAShapeLayer()
+    
+    @IBOutlet weak var imgVIew: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //setUpViewonImage()
+        
+        
+    }
+    
+//    func setUpViewonImage() {
+//        let view = UIView(frame: imgVIew.bounds)
+//        shapeLayer.fillColor = UIColor.white.withAlphaComponent(0.5).cgColor
+//
+//        bezierPath.move(to: .zero)
+//        bezierPath.addLine(to: CGPoint(x: 150, y: 0))
+//        bezierPath.addLine(to: CGPoint(x: view.frame.maxX, y: 30))
+//        bezierPath.addLine(to: CGPoint(x: view.frame.maxX, y: view.frame.maxY))
+//        bezierPath.addLine(to: CGPoint(x: 0, y: view.frame.maxY))
+//        bezierPath.addLine(to: .zero)
+//
+//        shapeLayer.path = bezierPath.cgPath
+//        shapeLayer.frame = view.bounds
+//
+//        view.layer.addSublayer(shapeLayer)
+//        imgVIew.addSubview(view)
+//        imgVIew.bringSubviewToFront(view)
+//    }
 }
